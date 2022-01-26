2e8e6494d6fae396904798780f90abb2eb727c28
Preston Darrough <techdarrough@gmail.com>
2022-01-25 19:55:21 -0800

    second commit

--- newNonPlayableCharacter.js
+++ newNonPlayableCharacter.js
@@ -23,9 +23,13 @@ function newNonPlayableCharacter(x, y) {
 
     setInterval(moveCharacter, 1)
 
-    function walkEast() {
+    function walkEast(time) {
         direction = 'east'
         element.src = `./assets/red-character/east.gif`
+        setTimeout((tim)=> 
+            {stop(),
+               if(callback){callback()}
+            }, time)
     }
 
     function walkNorth() {
